3271177d12a0e5be9fb8ba4fa48cae096cb60688
louis <Louis.Kelly1@hotmail.com>
2021-02-23 12:18:41 +0000

    Added composer package features

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9f5448434753ff6406bb9b29f4744b4a9c2c3209',
+    'name' => 'loukel/goodreads',
+  ),
+  'versions' => 
+  array (
+    'loukel/goodreads' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9f5448434753ff6406bb9b29f4744b4a9c2c3209',
+    ),
+  ),
+);
